1d0e9bcfc65fc5fbb875f83ffeb7e47cdedb9d77
martin1917 <63655028+martin1917@users.noreply.github.com>
2020-11-19 18:36:56 +0300

    Update Convertor.java

--- src/Convertor.java
+++ src/Convertor.java
@@ -2,20 +2,20 @@ import java.util.ArrayList;
 
 public class Convertor {
 	public ArrayList<Page> convertToPages(Song song, int countNontesOnPage){
-		ArrayList<Page> pages = new ArrayList<>(); //список страниц
-		int i; //номер страницы
-		for(i = 0; (i+1)*countNontesOnPage <= song.getNotes().size(); i++) { //пока суммарное кол-во нот на —“–јЌ»÷ј’ < всех нот в песне
-			Page page = new Page(countNontesOnPage); //создаем новую страинцу
+		ArrayList<Page> pages = new ArrayList<>();
+		int i; 
+		for(i = 0; (i+1)*countNontesOnPage <= song.getNotes().size(); i++) { 
+			Page page = new Page(countNontesOnPage); 
 
-			if(pages.size() > 0) pages.get(i-1).setNextPage(page); //если уже есть страницы, то сделать ссылку на текущую страницу
-			for(int j = i*countNontesOnPage; j < (i+1)*countNontesOnPage; j++) { //заполн€ем страницу
-				page.add(song.getNotes().get(j)); //добавл€ем страницу в список
+			if(pages.size() > 0) pages.get(i-1).setNextPage(page); 
+			for(int j = i*countNontesOnPage; j < (i+1)*countNontesOnPage; j++) { 
+				page.add(song.getNotes().get(j)); 
 			}
-			if(pages.size() > 0) page.setPrevPage(pages.get(i-1)); ////если уже есть страницы, то сделать ссылку на пред. страницу
+			if(pages.size() > 0) page.setPrevPage(pages.get(i-1)); 
 			pages.add(page);
 		}
 		
-		if(song.getNotes().size() % countNontesOnPage != 0) {//если остались ноты, не заполн€ющие страницу полностью, то запиываем сколько осталось на новую страницу
+		if(song.getNotes().size() % countNontesOnPage != 0) {
 			Page page = new Page(countNontesOnPage);
 			if(pages.size() > 0) pages.get(i-1).setNextPage(page);
 			for(int j = song.getNotes().size() - song.getNotes().size() % countNontesOnPage; j < song.getNotes().size(); j++) {
